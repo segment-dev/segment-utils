1387f7b1ec2b619ae3c5ee1bb8b17aaa48f94676
vincent.kurta <vincent.kurta@datadoghq.com>
2022-11-05 16:53:07 +0100

    Revert "adding an export command and a non-interactive mode using clap"
    This reverts commit e510a077c276bb26e8c672888c443fdaa8041983.

--- src/bin/segment-cli.rs
+++ src/bin/segment-cli.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use rustyline::{error::ReadlineError, Editor};
-use clap::Parser;
 use segment_rs::{
     client::Client,
     command::{Command, CommandError},
@@ -8,55 +7,32 @@ use segment_rs::{
 };
 use segment_utils::tokenizer;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    not_interactive: bool,
-    command: Option<Vec<String>>
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::parse();
-
     let client = Client::new(ConnectionOptions::new("127.0.0.1", 1698));
     let mut conn = client.get_connection().await?;
     let mut rl = Editor::<()>::new()?;
 
-    if args.not_interactive {
-        match args.command {
-            Some(command) => {
-                let line = command.join(" ");
+    loop {
+        let line = rl.readline(">> ");
+        match line {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
                 if let Ok(tokens) = tokenizer::tokenize(line) {
                     execute_command(tokens, &mut conn).await;
                 } else {
                     println!("malformed input received")
-                }            },
-            None => {println!("No command found, exiting.")}
-        }
-    } else {
-        loop {
-            let line = rl.readline(">> ");
-            match line {
-                Ok(line) => {
-                    rl.add_history_entry(line.as_str());
-                    if let Ok(tokens) = tokenizer::tokenize(line) {
-                        execute_command(tokens, &mut conn).await;
-                    } else {
-                        println!("malformed input received")
-                    }
                 }
-                Err(ReadlineError::Interrupted) => {
-                    break;
-                }
-                Err(ReadlineError::Eof) => {
-                    break;
-                }
-                Err(err) => {
-                    println!("Error: {:?}", err);
-                    break;
-                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
@@ -118,19 +94,6 @@ async fn execute_command(tokens: Vec<String>, conn: &mut Connection) {
                 for (i, val) in vec.iter().enumerate() {
                     print!("{}) ", i + 1);
                     print_string_result(val)
-                }
-            }
-            Err(e) => print_error_result(e),
-        },
-        "export" => match cmd.query::<Vec<String>>(conn).await {
-            Ok(vec) => {
-                if vec.is_empty() {
-                    println!("(empty list)");
-                } else {
-                    for val in vec.iter() {
-                        let values: Vec<&str> = val.split(",").collect();
-                        println!("segment-cli -n create {} evictor {};", values[0], values[1]);
-                    }
                 }
             }
             Err(e) => print_error_result(e),
